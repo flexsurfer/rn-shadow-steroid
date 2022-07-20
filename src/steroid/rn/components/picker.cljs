7a3151d6c639db1642bfab791bedb01991070138
tentoshka <lnvdmitry@outlook.com>
2022-07-21 02:57:42 +0700

    Updated dependencies and README

--- src/steroid/rn/components/picker.cljs
+++ src/steroid/rn/components/picker.cljs
@@ -1,6 +1,6 @@
 (ns steroid.rn.components.picker
   (:require [reagent.core :as reagent]
-            ["@react-native-community/picker" :refer (Picker)]))
+            ["@react-native-picker/picker" :refer (Picker)]))
 
 (def picker (reagent/adapt-react-class Picker))
 (def item (reagent/adapt-react-class (.-Item Picker)))
